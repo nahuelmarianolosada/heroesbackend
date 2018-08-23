e64b63d4a3fdbf94ee0f0a17eced1dce4024d1a8
nahuelmarianolosada <yosoyyo.nahuel@gmail.com>
2018-08-23 12:14:50 -0300

    JWT returns user in body like json

--- src/main/java/ar/com/heroes/config/JWTAuthenticationFilter.java
+++ src/main/java/ar/com/heroes/config/JWTAuthenticationFilter.java
@@ -1,20 +1,18 @@
 package ar.com.heroes.config;
 
 import ar.com.heroes.model.domain.user.UserEntity;
-import ar.com.heroes.services.user.IUserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.stereotype.Component;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -22,6 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 
 import static ar.com.heroes.config.Constants.*;
@@ -54,20 +53,39 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
                                             Authentication auth) throws IOException, ServletException {
 
-
         String token = Jwts.builder().setIssuedAt(new Date()).setIssuer(ISSUER_INFO)
                 .setSubject(((User)auth.getPrincipal()).getUsername())
                 .setExpiration(new Date(System.currentTimeMillis() + TOKEN_EXPIRATION_TIME))
                 .signWith(SignatureAlgorithm.HS512, SUPER_SECRET_KEY).compact();
 
-        String text = ((User) auth.getPrincipal()).getAuthorities().toString();
+        String email = ((User) auth.getPrincipal()).getUsername().toString();
+        Collection<GrantedAuthority> roles = ((User) auth.getPrincipal()).getAuthorities();
+
+        String userJson = getUserData(email, roles).toJSONString();
+
         response.setContentType("text/plain");  // Set content type of the response so that jQuery knows what it can expect.
         response.setCharacterEncoding("UTF-8"); // You want world domination, huh?
-        response.getWriter().write(text);
+        response.getWriter().write(userJson);
 
         response.addHeader("access-control-expose-headers", HEADER_AUTHORIZACION_KEY);
         response.addHeader(HEADER_AUTHORIZACION_KEY, TOKEN_BEARER_PREFIX + " " + token);
 
 
     }
+
+
+
+    private JSONObject getUserData(String name, Collection<GrantedAuthority> credenciales){
+        JSONObject obj = new JSONObject();
+        obj.put("email", name);
+
+        JSONArray authorities = new JSONArray();
+        for (GrantedAuthority cred: credenciales) {
+            authorities.add(cred.toString());
+        }
+        obj.put("authorities", authorities);
+        return obj;
+    }
+
+
 }
